93abfc8b8756fe8739d1e7ee443dd6e4b3b6bee5
Teodor Tysklind <37851342+TeoC00l@users.noreply.github.com>
2020-09-01 23:59:52 +0200

    Scene set up

---
+++ FutureGamesWeek3/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,9 @@
+﻿//@Author: Teodor Tysklind | FutureGames | Teodor.Tysklind@FutureGames.nu
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
+public class CharacterMovement : MonoBehaviour
+{
+    
+}
